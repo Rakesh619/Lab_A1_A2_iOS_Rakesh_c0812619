ac023bbb8987cd3e3e0690ee71aac080ee698b24
Rakesh <c0812619@mylambton.ca>
2021-09-20 00:27:47 -0400

    Initial Commit

---
+++ Lab_A1_A2_iOS_Rakesh_c0812619/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab_A1_A2_iOS_Rakesh_c0812619
+//
+//  Created by Mac on 2021-09-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
